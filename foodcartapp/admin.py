9257c028982cc0e57b99e52e77bd8c9238614695
aqwarius2003 <00victorr@gmail.com>
2025-02-02 23:56:07 +0400

    Added redirection to the order manager

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import (
     Order,
@@ -11,6 +13,7 @@ from .models import (
     RestaurantMenuItem,
     ProductCategory,
 )
+from star_burger.settings import ALLOWED_HOSTS
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -143,4 +146,14 @@ class OrderAdmin(admin.ModelAdmin):
                 item.save()
             else:
                 item.save()
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET and url_has_allowed_host_and_scheme(
+            request.GET['next'],
+            ALLOWED_HOSTS
+        ):
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
 
